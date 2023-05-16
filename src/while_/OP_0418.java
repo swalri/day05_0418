79819685a2bb9af1f873f2a4ead265fba2b1b16a
swalri <keb1128ke@gmail.com>
2023-05-16 14:55:04 +0900

    추가0516

--- src/while_/OP_0418.java
+++ src/while_/OP_0418.java
@@ -314,55 +314,55 @@ public class OP_0418 {
 //			System.out.println("소수가 아니다");
 //		}
 		//4번
-//		final int skill=20,nomal=10;
-//		int count = 10, mp = 100;
-//		int button_S;
-//		double hp = Math.floor( Math.random() * 150+50 );
-//		boolean tf = true;
-//		
-//		System.out.println("게임을 시작합니다");
-//		System.out.println("적의 hp 는 "+hp+"입니다");
-//		
-//		while(tf) {
-//			System.out.println();
-//			System.out.println("당신의 남은 체력은 "+count+"남았습니다.");
-//			System.out.println("당신의 남은 mp는 "+mp+"남았습니다.");
-//			System.out.println();
-//			System.out.println("공격수단을 선택해 주세요");
-//			System.out.println("1. 일반공격(10데미지)");
-//			System.out.println("2. 스킬사용(20데미지)");
-//			button_S = scan.nextInt();
-//			
-//			if(button_S == 1) {
-//				System.out.println("일반공격");
-//				hp -= nomal;
-//			}else if(button_S == 2) {
-//				System.out.println("스킬사용");
-//				if(mp < skill) {
-//					System.out.println("mp가 부족하여 스킬사용이 불가능 합니다");
-//				}else {
-//					mp -= skill;
-//					hp -= skill;
-//				}
-//			}else {
-//				System.out.println("오류 다시 입력해주세요");
-//			}
-//			
-//			System.out.println("적의 남은 hp : "+hp);
-//			
-//			if(hp <=0) {
-//				System.out.println("적이 쓰러졌습니다");
-//				System.out.println("당신이 승리하였습니다");
-//				tf=false;
-//			}else {
-//				count--;
-//			}
-//			
-//			if(count==0) {
-//				tf=false;
-//				System.out.println("패배하였습니다");
-//			}
-//		}//while
+		final int skill=20,nomal=10;
+		int count = 10, mp = 100;
+		int button_S;
+		double hp = Math.floor( Math.random() * 150+50 );
+		boolean tf = true;
+		
+		System.out.println("게임을 시작합니다");
+		System.out.println("적의 hp 는 "+hp+"입니다");
+		
+		while(tf) {
+			System.out.println();
+			System.out.println("당신의 남은 체력은 "+count+"남았습니다.");
+			System.out.println("당신의 남은 mp는 "+mp+"남았습니다.");
+			System.out.println();
+			System.out.println("공격수단을 선택해 주세요");
+			System.out.println("1. 일반공격(10데미지)");
+			System.out.println("2. 스킬사용(20데미지)");
+			button_S = scan.nextInt();
+			
+			if(button_S == 1) {
+				System.out.println("일반공격");
+				hp -= nomal;
+			}else if(button_S == 2) {
+				System.out.println("스킬사용");
+				if(mp < skill) {
+					System.out.println("mp가 부족하여 스킬사용이 불가능 합니다");
+				}else {
+					mp -= skill;
+					hp -= skill;
+				}
+			}else {
+				System.out.println("오류 다시 입력해주세요");
+			}
+			
+			System.out.println("적의 남은 hp : "+hp);
+			
+			if(hp <=0) {
+				System.out.println("적이 쓰러졌습니다");
+				System.out.println("당신이 승리하였습니다");
+				tf=false;
+			}else {
+				count--;
+			}
+			
+			if(count==0) {
+				tf=false;
+				System.out.println("패배하였습니다");
+			}
+		}//while
 	}
 }
 
